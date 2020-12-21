6d9aaee2a461a49fe059d2200d72dde15f0b50fa
ianclemence <batricupid@gmail.com>
2020-12-21 13:21:34 +0300

    Initial commit for previous files

--- analyzing_twitter_data.py
+++ analyzing_twitter_data.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 # # # # TWITTER CLIENT # # # #
-class TwittreClient():
+class TwitterClient():
     def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
